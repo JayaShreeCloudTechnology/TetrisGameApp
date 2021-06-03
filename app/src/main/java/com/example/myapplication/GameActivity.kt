a0202c70bf7559d0c07035eb35fef2468d260602
jacso <dudin10@mail.ru>
2021-06-03 14:51:47 +0300

    new Frame.kt, HelperFunctions.kt

--- app/src/main/java/com/example/myapplication/GameActivity.kt
+++ app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -1,12 +1,34 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import storage.AppPreferences
 
+var tvHighScore: TextView? = null
+var tvCurrentScore: TextView? = null
+var appPreferences: AppPreferences? = null
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        appPreferences = AppPreferences(this)
+
+        val btnRestart = findViewById<Button>(R.id.btn_restart)
+        tvHighScore = findViewById(R.id.tv_high_score)
+        tvCurrentScore = findViewById(R.id.tv_current_score)
+
+        updateHighScore()
+        updateCurrentScore()
+    }
+
+    private fun updateHighScore() {
+        tvHighScore?.text = "${appPreferences?.getHighScore()}"
+    }
+
+    private fun updateCurrentScore() {
+        tvCurrentScore?.text = "0"
     }
 }
